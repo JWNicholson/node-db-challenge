363c49d0a0f3d41ae2aa9a3d8be3cbc4e3f0b635
JW Nicholson <john.nicholson812@gmail.com>
2020-04-17 12:08:51 -0400

    project model skeleton complete

---
+++ projects/projects-model.js
@@ -0,0 +1,58 @@
+const db = require('../data/db-config');
+
+const knex = require('knex');
+
+module.exports = {
+    getProjects,
+    getProjectById,
+    getAllTasks,
+    getTaskById,
+    getResources,
+    getResourceById,
+    getProjectTasks,
+    addProject,
+    addResource,
+    addTask
+}
+
+//Projects
+function getProjects(){
+
+}
+
+function getProjectById(){
+
+}
+
+function addProject(){
+    
+}
+//Tasks
+function getAllTasks(){
+
+}
+
+function getTaskById(){
+
+}
+
+function addTask(){
+
+}
+
+//Resources
+function getResources(){
+
+}
+
+function getResourceById(){
+
+}
+
+function getProjectTasks(){
+
+}
+
+function addResource(){
+    
+}
--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express');
 
-//const projectsRouter = require('./projects/projects.router');
+const projectsRouter = require('./projects/projects.router');
 
 const server = express();
 
 server.use(express.json());
 
-//server.use('./api', projectsRouter);
+server.use('./api', projectsRouter);
 
 server.get('/', (req,res) => {
     res.send("<h1>This is the Node DB Sprint</h1>")
