1dc951980c34085ffb74aa06a633f329c1a607ed
JW Nicholson <john.nicholson812@gmail.com>
2020-04-17 12:16:41 -0400

    projects-router.js skeleton finished

---
+++ projects/projects.router.js
@@ -0,0 +1,44 @@
+const express = require('express');
+
+const Projects = require('./projects-model');
+
+const router = express.Router();
+
+//Projects//
+
+//GET all projects
+
+
+//Get by Id
+
+
+//Add project
+
+
+//Tasks//
+
+//Get all tasks
+
+
+//Get by Id
+
+
+//Get a projects tasks
+
+
+//Add a task to project
+
+
+//Resources//
+
+//Get all resources
+
+
+//Get by Id
+
+
+//Add resource
+
+
+
+module.exports = router;
